2a1739c8541c6c6fe29102d951e32552c64d4184
zasu18 <zasu18@proton.me>
2024-02-17 12:07:31 +0530

    minor front end fixes

--- packages/nextjs/pages/groups1.tsx
+++ packages/nextjs/pages/groups1.tsx
@@ -5,10 +5,10 @@ import logo from '../public/logo.jpeg';
 import Image from "next/image";
 const Home: NextPage = () => {
   const data = [
-  "93433770077998496254",
-  "464186796256987445622",
-  "54245599003344298293",
-  "631247691329682424424"
+  "93433770077998496254123456789",
+  "464186796256987445622123456785",
+  "542455990033442982933246465365",
+  "631247691329682424453536573724"
     ];
 
   return (
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
             <ul className="space-y-2 font-medium">
               <li>
-                  <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                  <a href="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <Image
                           className="w-8 h-8 "
                           src={logo}
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
                   </a>
               </li>
               <li>
-                  <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                  <a href="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                         <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
                     </svg>
@@ -120,8 +120,8 @@ const Home: NextPage = () => {
         <div className="px-4 py-4 bg-gray-800 mb-4 rounded">
             <div className="flex flex-col">
                 <button className="btn mb-2">Join Group</button>
-                <div className="overflow-x-auto">
-                <table className="table">
+                <div className="overflow-x-auto ">
+                <table className="table w-full min-w-full">
                     <thead>
                     <tr>
                         <th>Users</th>
@@ -130,7 +130,11 @@ const Home: NextPage = () => {
                     <tbody>
                         {data.map((group, index) => (
                         <tr key={index}>
-                            <td>{group}</td>
+                             <td>
+                        <p className="font-bold font-xl text-lg">
+                           {group}
+                        </p>
+                        </td>
                         </tr>
                         ))}
                     </tbody>
--- packages/nextjs/pages/groups2.tsx
+++ packages/nextjs/pages/groups2.tsx
@@ -47,7 +47,7 @@ const Home: NextPage = () => {
     "Semaphore is cool :)",
     "Hello :)",
     "testing",
-    "Love semaphore! Hello friends"
+    "Love semaphore! Hello friends. Good to see you all"
         ];
 
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -80,7 +80,7 @@ const Home: NextPage = () => {
                   </a>
               </li>
               <li>
-                  <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                  <a href="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
                         <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
                     </svg>
@@ -181,7 +181,12 @@ const Home: NextPage = () => {
                     <tbody>
                         {data.map((group, index) => (
                         <tr key={index}>
-                            <td>{group}</td>
+
+                            <td>
+                                <p className="font-bold font-xl text-lg">
+                                    {group}
+                                    </p>
+                            </td>
                         </tr>
                         ))}
                     </tbody>
